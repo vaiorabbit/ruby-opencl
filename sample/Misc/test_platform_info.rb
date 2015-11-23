b696d3300215c7199762fc1dfbcf26d06c89afdb
UK <uk@Macmini-UK.local>
2015-11-23 12:51:35 +0900

    sample/Misc: Moved test_*.rb here.

--- sample/Misc/test_platform_info.rb
+++ sample/Misc/test_platform_info.rb
@@ -1,8 +1,12 @@
-require_relative '../lib/opencl'
+require_relative '../../lib/opencl'
 
-# OpenCL.load_lib('/System/Library/Frameworks/OpenCL.framework/OpenCL') # For Mac OS X
-# OpenCL.load_lib('c:/Program Files/NVIDIA Corporation/OpenCL/OpenCL64.dll') # For Windows x86-64 NVIDIA GPU (* comes with NVIDIA Driver)
-OpenCL.load_lib('c:/Windows/System32/OpenCL.dll') # For Windows
+# Load DLL
+begin
+  OpenCL.load_lib('c:/Windows/System32/OpenCL.dll') # For Windows
+rescue
+  OpenCL.load_lib('/System/Library/Frameworks/OpenCL.framework/OpenCL') # For Mac OS X
+end
+
 cl_platforms_buf = ' ' * 4
 
 OpenCL.clGetPlatformIDs(1, cl_platforms_buf, nil)
